d14936b6b204311062c6573ce016f9adc0803084
wang1128 <penghao@pal-nat184-021-067.itap.purdue.edu>
2015-10-03 16:12:13 -0400

    4:12

--- PenghaoWang_HW2.py
+++ PenghaoWang_HW2.py
@@ -6,7 +6,6 @@ import csv
 import sys
 from collections import Counter
 
-
 def remove_values_from_list(the_list, val):
    return [value for value in the_list if value != val]
 
@@ -17,8 +16,9 @@ f = open('train.csv')
 csv_f = csv.reader(f)
 review_list =[]
 list_word=[]
+list_word2=[]
 all_word=[]
-
+al_w=set()
 for row in csv_f:
     if row[0].split(' ') not in review_list and row[0].split(' ') != ',':
         review_list.append(row[0].split(' '))
@@ -29,41 +29,29 @@ for i in range(review_list.__len__()):
     for column in review_list[i]:
         if column != ',' and column != '.' and column !=''and column !='!'and column !='...' :
             all_word.append(column)
-            if column not in list_word:
-                list_word.append(column)
+            al_w.add(column)
+            #if column not in list_word2:
+            #    list_word2.append(column)
+#print(all_word[0])
+#print(al_w)
+#print(list_word2)
+list_word=list(al_w)
 
-
-print(all_word.__len__())
-ad_list=remove_values_from_list(all_word,'a')
 print(list_word.__len__())
-print(all_word.count('a'))
-print(ad_list.__len__())
+#print(list_word2.__len__())
+#temp = [item for item in list_word if item in list_word2]
+#print(temp.__len__())
+#ad_list=remove_values_from_list(all_word,'a')
+#print(list_word.__len__())
+#print(all_word.count('a'))
+#print(ad_list.__len__())
 
-"""
-for word in all_word:
-    if all_word.count(word)>500:
-        print(word,all_word.count(word))
-        all_word=remove_values_from_list(all_word,word)
-
-"""
-
-#if review_list[0][1] == 'messages':
-#    print('cool')
-#print(list_word.count("a"))
 
 c= Counter(all_word).most_common(100)
-print (c)
+c1=c[1]
+print (c1[0])
 
+for elements in c:
+    removestr = elements
+    print (removestr)
 
-
-#my_string = "blah, lots  ,  of ,  spaces, here "
-#mylist = [x.strip() for x in my_string.split(',')]
-#print(mylist[1])
-#print(review_list)
-#print(csv_headings[0])
-#for word in f.read().split():
-#    print(word)
-
-#traindata = loadtxt('train.csv',usecols=range(0),dtype=str)
-
-#print(traindata)
