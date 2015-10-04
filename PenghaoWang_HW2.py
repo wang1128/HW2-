f3cde1ee4bcf5d1c4d3f36e6669b641529ac26c7
wang1128 <penghao@penghaos-MacBook-Air.local>
2015-10-03 22:02:33 -0400

    10:02

--- PenghaoWang_HW2.py
+++ PenghaoWang_HW2.py
@@ -17,8 +17,8 @@ al_w=set()
 for row in csv_f:
     if row[0].split(' ') not in review_list and row[0].split(' ') != ',':
         review_list.append(row[0].split(' '))
-
 #Creat the list of words that we will use
+#print (review_list[0])
 for i in range(review_list.__len__()):
     for column in review_list[i]:
         if column != ',' and column != '.' and column !=''and column !='!'and column !='...'and column !='(' and column !=')'and column !='"'and column !='--'and column !='?' \
@@ -27,10 +27,23 @@ for i in range(review_list.__len__()):
             al_w.add(column)
 
 list_word=list(al_w)
-
 #Delete the most common words and words that appears less than five time
 c= Counter(all_word).most_common(list_word.__len__())
 for elements in c:
     removestr = elements
     if removestr[1]<5 or removestr[1]>300:
         list_word.remove(removestr[0])
+
+w = [0]*list_word.__len__()
+x = [0]*list_word.__len__()
+print(w.__len__())
+print(list_word.__len__())
+#print(list_word.index('clearly'))
+for words in review_list[0]:
+    if words in list_word:
+        index =list_word.index(words)
+        print(index)
+        x[index] = 1
+        print(words)
+print(x[1451])
+print(list_word[1451])
