5ac926e2559dc935492bb82ab46a21059d441434
wang1128 <penghao@pal-nat184-004-046.itap.purdue.edu>
2015-10-04 22:12:15 -0400

    10:12

--- PenghaoWang_HW2.py
+++ PenghaoWang_HW2.py
@@ -24,12 +24,8 @@ for row in csv_f:
     if row[1] is'+':
         review_label.append(1)
     else :
-        review_label.append(0)
+        review_label.append(-1)
 
-print(review_label)
-#print(review_label)
-#Creat the list of words that we will use
-#print (review_list[0])
 for i in range(review_list.__len__()):
     for column in review_list[i]:
         if column != ',' and column != '.' and column !=''and column !='!'and column !='...'and column !='(' and column !=')'and column !='"'and column !='--'and column !='?' \
@@ -45,27 +41,56 @@ for elements in c:
     if removestr[1]<5 or removestr[1]>300:
         list_word.remove(removestr[0])
 
-w = [0]*list_word.__len__()
+
 
 feature_array = np.zeros((review_list.__len__(),list_word.__len__()))
-#print(w.__len__())
-#print(list_word.__len__())
-#print(list_word.index('clearly'))
-#creat feature value for every row
+
+
+#feature_array contain all x, x is features of every row
 for idx in range(review_list.__len__()):
     x = [0]*list_word.__len__()
     for words in review_list[idx]:
         if words in list_word:
             index =list_word.index(words)
             x[index] = 1
-            feature_array[idx] = np.array(x)
-#print(list_word[1451])
-print(feature_array[0])
-y = np.array(list_word)
-print(y)
-print type(y)
+            feature_array[idx] = np.array(x,dtype=int)
 
-maxIter = 10
-b=0
-for iteration in range(maxIter):
-    random_idx = np.random.permutation(2853)
+
+def perceptron(maxIter):
+    w = np.zeros(list_word.__len__())
+    maxIter = maxIter
+    b=0
+    for iteration in range(maxIter):
+        w_copy =w
+        random_idx = np.random.permutation(review_list.__len__())
+        for row_idx in random_idx:
+            y = np.dot(w,feature_array[row_idx]) + b
+            if y>0:
+                predict_label = 1
+            else:
+                predict_label = -1
+            review_l = review_label[row_idx]
+            if review_l != predict_label:
+                w = w + review_l* feature_array[row_idx]
+        if np.array_equal(w_copy,w):
+            print("converge"+ iteration)
+            continue
+    return w
+
+print(perceptron(10))
+'''
+f = open('validation.csv')
+csv_f2 = csv.reader(f)
+review_list =[]
+review_label = []
+list_word=[]
+all_word=[]
+al_w=set()
+for row in csv_f2:
+    if row[0].split(' ') not in vreview_list and row[0].split(' ') != ',':
+        review_list.append(row[0].split(' '))
+    if row[1] is'+':
+        vreview_label.append(1)
+    else :
+        vreview_label.append(-1)
+ '''
